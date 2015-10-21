041678453d109b59856a0029f66e8da2c825fdd0
Tim Hettler <me@timhettler.com>
2015-10-21 16:28:51 -0400

    inital commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#SVGs Are A Thing Now
+A quick tour of the Whys &amp; Hows of using SVG in front-end development.
---
+++ app.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+
+    build_dir: 'build',
+    compile_dir: 'dist',
+
+    meta: {
+        title: 'SVGs Are a Thing Now',
+        description: 'A quick tour of the Whys &amp; Hows of using SVG in front-end development.',
+        viewport: 'width=device-width, initial-scale=1, user-scalable=no, minimal-ui'
+    },
+
+    app_files: {
+        main: ['slide.js'],
+        js: ['src/js/**/*.js'],
+        html: ['src/*.html', 'src/slides/**/*.html'],
+        styles: ['src/sass/**/*']
+    },
+
+    vendor_files: {
+        js: [
+            'bower_components/zepto/zepto.js',
+            'vendor/prism/prism.js'
+        ],
+        css: [
+            'bower_components/sanitize-css/dist/sanitize.css',
+            'vendor/prism/prism.css',
+        ]
+    }
+};
